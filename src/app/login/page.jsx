dd29d9719365cf76345721471276d7352818a83d
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-11-21 18:49:22 -0300

    construção tela login

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/componentes/LoginForm"
+
+    const LoginPage = () => {
+        return(
+            <div>
+                <h1>Login</h1>
+                <LoginForm/>
+            </div>
+        )
+}
+export default LoginPage
