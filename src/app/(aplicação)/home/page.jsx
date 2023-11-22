6b139ef86dc30e695ec05679bb48f59eec8a4d6e
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-11-22 10:30:53 -0300

    construçãop componente pg home

--- src/app/(aplicação)/home/page.jsx
+++ src/app/(aplicação)/home/page.jsx
@@ -1,10 +1,33 @@
+import Noticia from "@/componentes/Noticia/noticia"
 
-'use client'
+const Noticias = [
+    {
+        id: 1,
+        titulo:'Noticia 1',
+        img:'http://via.placeholder.com/150',
+        texto:'Texto da <b>noticia</b>'
+    },{
+        id: 2,
+        titulo:'Noticia 2',
+        img:'http://via.placeholder.com/150',
+        texto:'Texto da <b>noticia</b>'
+    },{
+        id: 3,
+        titulo:'Noticia 3',
+        img:'http://via.placeholder.com/150',
+        texto:'Texto da <b>noticia</b>'
+    }
+
+]
 
 const HomePage = () => {
     return (
         <div>
             <h1>Home</h1>
+            { Noticias.map(noticia =>
+            <Noticia key={ noticia.id } noticia={ noticia } />)
+
+            }
         </div>
     )
 }
